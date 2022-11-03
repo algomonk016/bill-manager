e4a44817afc07ce62d0da920396606a3db7ebc95
algomonk016 <stshandilya0820@gmail.com>
2022-11-03 16:53:16 +0530

    Level 2 completed

--- src/components/Dashboard/BudgetTable.tsx
+++ src/components/Dashboard/BudgetTable.tsx
@@ -1,11 +1,9 @@
-import { AddCardTwoTone } from "@mui/icons-material";
-import { Typography, Grid } from "@mui/material";
+
 import { ColDef, GridOptions } from "ag-grid-community";
 import { AgGridReact } from "ag-grid-react";
 import React, { useMemo, useRef } from "react";
 import { columnDef, Bill } from '../../constants'
-import BillForm from "./BillForm";
-import Modal from "../Modal";
+
 
 interface Props {
   data: Bill[]
@@ -36,17 +34,6 @@ const BudgetTable = (props: Props): JSX.Element => {
 
   return (
     <div style={containerStyle} >
-      <Grid container justifyContent={'space-between'} alignItems={'center'} mb={1} >
-        <Typography typography={'caption'} fontSize={'20px'} > Monthly Bill </Typography>
-
-        <Modal
-          title="Add Bill Details"
-          text={ <AddCardTwoTone color="success" /> }
-          content={<BillForm />}
-          icon
-        />
-      </Grid>
-
       <div className="ag-theme-alpine" style={gridStyle}>
         <AgGridReact
           ref={gridRef}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,21 +1,97 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "@mui/material";
-import { BudgetTable } from "../components";
 import { store } from "../redux/store";
 import { fetchBillsDetails } from "../redux/store/slices/billSlice";
 import { useSelector } from "react-redux";
+import { BillForm, Modal, BudgetTable } from "../components";
+import { AddCardSharp, CalculateTwoTone, CurrencyRupeeOutlined } from "@mui/icons-material";
+import { Typography, Grid, Paper, InputBase, Divider, IconButton } from "@mui/material";
+import { Bill } from "../constants";
+import { sortByKey } from "../utils/helper";
 
 const Dashboard = (): JSX.Element => {
-
+  const [monthlyBudget, setMonthlyBudget] = useState<number>(0)
+  const [billsAccToBudget, setBillsAccToBudget] = useState<any>()
+  const [filterBills, setFilterBills] = useState<boolean>(false)
   const data = useSelector((state: any) => state?.bills?.data)
 
   useEffect(() => {
     store.dispatch(fetchBillsDetails())
   }, [])
 
+  const filterBillsAccToBudget = () => {
+    if(monthlyBudget === 0){
+      setFilterBills(false)
+      return;
+    }
+
+    let newData = [...data.bills].map((data: Bill) => {
+      return {
+        id: data.id,
+        amount: Number(data.amount)
+      }
+    })
+
+    sortByKey(newData, 'amount', true)
+
+    setBillsAccToBudget(getBillsFromIds(getMinimumIds(newData)))
+    setFilterBills(true)
+  }
+
+  const getBillsFromIds = (ids: number[]) => {
+    return data.bills.filter((dt: any) => ids.includes(dt.id))
+  }
+
+  const getMinimumIds = (data: {id: number, amount: number}[]): number[] => {
+    let ids: number[] = [];
+
+    let currentBudget = monthlyBudget;
+
+    for(const bill of data){
+      const { id, amount } = bill;
+      if(amount < currentBudget){
+        currentBudget -= amount;
+        ids.push(id);
+      }
+    }
+
+    return ids;
+  }
+
   return (
     <Container>
-      <BudgetTable data={data?.bills} />
+      <Grid container justifyContent={'space-between'} alignItems={'center'} mb={2} >
+        <Typography typography={'caption'} fontSize={'20px'} > Monthly Bill </Typography>
+
+        <Grid display={'flex'} alignItems={'center'} width={300} justifyContent={'space-between'} >
+          <Paper
+            component="form"
+            sx={{ p: '3px 3px', display: 'flex', alignItems: 'center', width: 230, height: 35 }}
+          >
+            <CurrencyRupeeOutlined />
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Monthly Budget"
+              onChange={(e) => setMonthlyBudget(Number(e.target.value))}
+              value={monthlyBudget === 0 ? '' : monthlyBudget}
+              inputProps={{ 'aria-label': 'monthly budget' }}
+            />
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton onClick={filterBillsAccToBudget} color="primary" sx={{ p: '10px' }} aria-label="directions">
+              <CalculateTwoTone />
+            </IconButton>
+          </Paper>
+
+          <Modal
+            title="Add Bill Details"
+            text={<AddCardSharp color="success" />}
+            content={<BillForm />}
+            icon
+          />
+        </Grid>
+      </Grid>
+
+      <BudgetTable data={filterBills ? billsAccToBudget : data?.bills} />
     </Container>
   )
 }
